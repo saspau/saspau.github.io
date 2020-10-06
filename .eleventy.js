3f8b5b156fcff312e1609fe6c479a368d1e66fe2
Sasha Paulovich <Sasha.Paulovich@renaissance.com>
2020-10-05 23:33:55 -0400

    get njk to work and also add some potentially unnecessary packages

--- .eleventy.js
+++ .eleventy.js
@@ -2,16 +2,44 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
       markdownIt = require('markdown-it');
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.setTemplateFormats([
+    "md",
+    "html",
+    "txt",
+    "jpeg",
+    "jpg",
+    "png",
+    "GIF",
+    "wav",
+    "mp3"
+  ]);
+  eleventyConfig.addPassthroughCopy("style");
+  eleventyConfig.addPassthroughCopy("webfonts");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("CNAME");
+
   eleventyConfig.addPassthroughCopy('favicon.ico');
+
+  let markdownIt = require("markdown-it");
+  let markdownItDeflist = require("markdown-it-deflist");
+  let markdownItFootnote = require("markdown-it-footnote");
+  let markdownItAnchor = require("markdown-it-anchor");
+  let markdownItTocDoneRight = require("markdown-it-toc-done-right");
+  let markdownItAttrs = require("markdown-it-attrs");
+  let markdownItTaskLists = require("markdown-it-task-lists");
+  let markdownItEmoji = require("markdown-it-emoji");
+
   const options = {
     html: true,
     breaks: true,
     linkify: false
   };
-  eleventyConfig.setLibrary("md", markdownIt(options));
+  let markdownLib = markdownIt(options).use(markdownItDeflist).use(markdownItFootnote).use(markdownItAnchor).use(markdownItTocDoneRight).use(markdownItAttrs).use(markdownItTaskLists).use(markdownItEmoji);
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     // Use liquid in html templates
-    htmlTemplateEngine: "njk"
+    markdownTemplateEngine: "njk"
   };
 };
