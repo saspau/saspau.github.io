48a50fd7be7ad406771ce7a4d81baa929acf6aa2
sai chikine <sai.chikine@gmail.com>
2020-10-06 21:07:46 -0600

    Adding stuff for custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
       markdownIt = require('markdown-it');
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   eleventyConfig.setTemplateFormats([
     "md",
